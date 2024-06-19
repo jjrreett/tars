874f209cb36db8a6e9fb980948b96a5379d5a150
Jarrett Bullion <jjrreett@gmail.com>
2024-06-19 16:13:03 -0700

    Make target pos relitive to inital position

--- Generic6DOFJoint3D.gd
+++ Generic6DOFJoint3D.gd
@@ -2,7 +2,9 @@ extends Generic6DOFJoint3D
 
 var body_a
 var body_b
-var target_position = Vector3(2, 1, 0)
+var initial_pos
+
+var target_position = Vector3(0, 1, 0)
 var max_velocity = 2.0
 var max_force = 100.0
 var time = 0.0
@@ -10,19 +12,21 @@ var time = 0.0
 func _ready():
 	body_a = get_node(node_a) as RigidBody3D
 	body_b = get_node(node_b) as RigidBody3D
+	
+	initial_pos = body_b.transform.origin - body_a.transform.origin
 
 func _physics_process(delta):
 	time += delta
-	var delta_pos = body_b.transform.origin - body_a.transform.origin
+	var delta_pos = (body_b.transform.origin - body_a.transform.origin) - initial_pos
 	var delta_vel = body_b.linear_velocity - body_a.linear_velocity
 	
 	# Update the target position every second to go up and down
-	if int(time) % 2 == 0:
-		target_position = Vector3(1, 1, 0)
-	else:
-		target_position = Vector3(1, -1, 0)
-	
-	target_position = Vector3(1, cos(time), sin(time))
+	#if int(time) % 2 == 0:
+		#target_position = Vector3(1, 1, 0)
+	#else:
+		#target_position = Vector3(1, -1, 0)
+	#
+	target_position = Vector3(0, cos(time), sin(time))
 		
 	# Calculate the desired velocity to reach the target position
 	var position_error = target_position - delta_pos
