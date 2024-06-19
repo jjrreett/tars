b1b8bad52914de54923166264d7c7bf439dac1fa
Jarrett Bullion <jjrreett@gmail.com>
2024-06-19 16:14:22 -0700

    Tabs to spaces

--- Generic6DOFJoint3D.gd
+++ Generic6DOFJoint3D.gd
@@ -10,34 +10,29 @@ var max_force = 100.0
 var time = 0.0
 
 func _ready():
-	body_a = get_node(node_a) as RigidBody3D
-	body_b = get_node(node_b) as RigidBody3D
-	
-	initial_pos = body_b.transform.origin - body_a.transform.origin
+    body_a = get_node(node_a) as RigidBody3D
+    body_b = get_node(node_b) as RigidBody3D
+
+    initial_pos = body_b.transform.origin - body_a.transform.origin
 
 func _physics_process(delta):
-	time += delta
-	var delta_pos = (body_b.transform.origin - body_a.transform.origin) - initial_pos
-	var delta_vel = body_b.linear_velocity - body_a.linear_velocity
-	
-	# Update the target position every second to go up and down
-	#if int(time) % 2 == 0:
-		#target_position = Vector3(1, 1, 0)
-	#else:
-		#target_position = Vector3(1, -1, 0)
-	#
-	target_position = Vector3(0, cos(time), sin(time))
-		
-	# Calculate the desired velocity to reach the target position
-	var position_error = target_position - delta_pos
-	var target_velocity = (10 * position_error).limit_length(max_velocity)
-	var velocity_error = target_velocity - delta_vel
-	
-	# Calculate the force needed to achieve the desired velocity, clamped to the maximum force
-	var force = (100 * (velocity_error * body_b.mass)).limit_length(max_force)
-	
-	print("position_error=", position_error, " force=", force)
-	
-	# Apply the force to both bodies
-	body_a.apply_force(-force)
-	body_b.apply_force(force)
+    time += delta
+    var delta_pos = (body_b.transform.origin - body_a.transform.origin) - initial_pos
+    var delta_vel = body_b.linear_velocity - body_a.linear_velocity
+
+    # circle step
+    target_position = Vector3(0, cos(time), sin(time))
+       
+    # Calculate the desired velocity to reach the target position
+    var position_error = target_position - delta_pos
+    var target_velocity = (10 * position_error).limit_length(max_velocity)
+    var velocity_error = target_velocity - delta_vel
+
+    # Calculate the force needed to achieve the desired velocity, clamped to the maximum force
+    var force = (100 * (velocity_error * body_b.mass)).limit_length(max_force)
+
+    print("position_error=", position_error, " force=", force)
+
+    # Apply the force to both bodies
+    body_a.apply_force(-force)
+    body_b.apply_force(force)
